8dbd3068118fed47e1d87a87a7ae5238cf9e1e77
Jerome Martinon <jerome.martinon@outlook.com>
2018-06-28 10:32:15 +0200

    REST API OK. 3 routes: All, ByID and ComplexFilters

--- symfony/src/AppBundle/Controller/AttractionController.php
+++ symfony/src/AppBundle/Controller/AttractionController.php
@@ -8,18 +8,20 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AttractionController extends Controller
 {
     /**
      * @Rest\View()
-     * @Get("/attractions")
+     * @Route("/attractions")
+     * @Method({"GET"})
      */
     public function getAttractionsAction(Request $request)
     {
@@ -35,10 +37,52 @@ class AttractionController extends Controller
         return $attractions;
     }
 
+    /**
+     * @Rest\View()
+     * @Route("/attractions/{filter}", requirements={"filter"="[a-z\-\+\,\=]+\d+[a-z\-\+\,\=\d+]*"})
+     * @Method({"GET"})
+     */
+    public function getAttractionsByFilterAction($filter, Request $request)
+    {
+        $filters = $this->filtersTranslation($filter);
+        if(isset($filters['type'])){
+            $filters['type'] = $this->get('doctrine.orm.entity_manager')
+                ->getRepository('AppBundle:Type')
+                ->findByName($filters['type']);
+        }
+        if(isset($filters['risk'])){
+            $filters['risk'] = $this->get('doctrine.orm.entity_manager')
+                ->getRepository('AppBundle:Risk')
+                ->findByLevel($filters['risk']);
+        }
+
+        $attractions = $this->get('doctrine.orm.entity_manager')
+            ->getRepository('AppBundle:Attraction')
+            ->findBy($filters);
+
+        if (empty($attractions)) {
+            return new JsonResponse(['message' => 'Aucune attraction trouvée'], Response::HTTP_NOT_FOUND);
+        }
+
+        return $attractions;
+    }
+
+    private function filtersTranslation($filters)
+    {
+        $byCategory = explode("+",$filters);
+        $result = [];
+        foreach($byCategory as $value){
+            $currentFilter = explode("=",$value);
+            $result[$currentFilter[0]] = explode(",",$currentFilter[1]);
+        }
+        return $result;
+    }
+
 
     /**
      * @Rest\View()
-     * @Get("/attractions/{id}")
+     * @Route("/attractions/{id}", requirements={"id"="\d+"})
+     * @Method({"GET"})
      */
     public function getAttractionAction($id, Request $request)
     {
