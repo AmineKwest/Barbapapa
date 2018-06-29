b8be10185b442fa9da5d51e0e68d4749580eb17f
Pierrick <quetille.pierrick@gmail.com>
2018-06-29 09:34:11 +0200

    mapping through the datas

--- front/src/Components/MapPark.jsx
+++ front/src/Components/MapPark.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {Map, Marker, Popup, ImageOverlay} from 'react-leaflet';
 import L from 'leaflet';
-// import { ImageOverlay } from 'leaflet'
 import './MapPark.css';
 
 import greenBeard from './beard_green.png';
@@ -14,29 +13,31 @@ class MapPark extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      zoom: 14
+      zoom: 14,
+      attractions: []
     }
     this.bounds = [[40.71, -74.25], [40.77, -74.12544]];
     this.center = [40.74, -74.19];
-    this.croisiere = [40.753, -74.177];
+    this.croisiere = [40.76, -74.168];
+    this.auberge = [40.746, -74.195];
     this.lancer = [40.732, -74.159];
     this.barber = [40.728, -74.229];
-    this.auberge = [40.736, -74.195];
     this.ring = [40.753, -74.215];
 
     this.icon = (risk) => {
       const choice = (p) => {
         switch (p) {
-          case 'tranquille':
+          case 'Tranquille':
             return greenBeard;
-          case 'dangereux':
+          case 'Dangereux':
             return orangeBeard;
-          case 'mortel':
+          case 'Mortel':
             return redBeard;
           default:
             return greenBeard;
         }
       }
+
       return new L.icon({
         iconUrl: choice(risk),
         iconSize:     [40, 40], // size of the icon
@@ -45,28 +46,39 @@ class MapPark extends Component {
       })
     }
   }
+  dataRender = (data, i, key) => data ? data[i].key : 'error';
+
+  componentWillMount() {
+    fetch('/attractions')
+    .then(res => res.json())
+    .then(json => this.setState({attractions: json})
+    )
+  
+  };
 
   render() {
+    
     return (<div>
       <Map center={this.center} zoom={this.state.zoom} minZoom={14} maxZoom={17} maxBounds={this.bounds} maxBoundsViscosity={0.9}>
         <ImageOverlay
           url={mapPic}
           bounds={this.bounds}>
-          <Marker position={this.croisiere} icon={this.icon('tranquille')}>
-            <Popup><h1>Le drakkar s'amuse</h1><p>Quand l'amour rencontre la barbe</p></Popup>
+          {this.state.attractions.map( (data, i) => 
+          <Marker key={i} position={[data.lat, data.long]} icon={this.icon(data.risk.level)}>
+            <Popup key={i}> 
+              <h1>{data.name}</h1> 
+              <img src={data.photo} alt={data.name} width='150px' height='auto'/>
+              <p>{data.description}</p>
+              <p>Prix : {data.price} euros</p>
+              <p>Ouverture: {data.openingTime}</p>
+              <p>Attente: {data.waitingtime} mn</p>
+              <p>Age minimal: {data.age}</p>
+              <p>Capacité: {data.capacity} personnes</p>
+              <p>Risque du moment: {data.risk.level}</p>
+              <p>Victimes actuelles: {data.victims}</p>
+            </Popup>
           </Marker>
-          <Marker position={this.auberge} icon={this.icon('tranquille')}>
-            <Popup><h1>L'auberge du poney qui tousse</h1><p>Le lieu de rencontre incontournable</p></Popup>
-          </Marker>
-          <Marker position={this.lancer} icon={this.icon('dangereux')} >
-            <Popup><h1>Le lancer de nains</h1><p>Testez votre puissance</p></Popup>
-          </Marker>
-          <Marker position={this.barber} icon={this.icon('mortel')} >
-            <Popup><h1>Sweeny's Barber</h1><p>Prenez le trésor au risque de votre barbe !</p></Popup>
-          </Marker>
-          <Marker position={this.ring} icon={this.icon('mortel')} >
-            <Popup><h1>Le ring de l'évolution</h1><p>Retour de bâton pour Darwin</p></Popup>
-          </Marker>
+          )}
         </ImageOverlay>
       </Map>
     </div>);
